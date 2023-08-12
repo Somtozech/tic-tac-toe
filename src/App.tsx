82b7246c3135b0e34716096c1d9eb56b4c1ccc30
Nnaemeka Ezechi <nnaemeka@lifebit.ai>
2023-08-12 01:03:27 +0100

    feat: setup firebase initialization

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import {RouterProvider} from "react-router-dom";
+import {router} from "./pages";
+
+import {initializeApp} from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+initializeApp(firebaseConfig);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
